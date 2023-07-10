4d97751b3756a7eaa9732db380e5684055b6c72c
cheuyin <yinstanleycheung@gmail.com>
2023-07-10 12:39:29 -0700

    Created sign up component, form input component, button component

---
+++ src/components/Button/Button.component.jsx
@@ -0,0 +1,25 @@
+import "./Button.styles.scss"
+
+/*
+Three types of buttons:
+- default
+- inverted
+- Google sign in
+*/
+
+const BUTTON_TYPE_CLASSES = {
+    google: "google-sign-in",
+    inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+    return (
+        <button
+            className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,5 +1,9 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/SignUpForm/SignUpForm.component";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -11,6 +15,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google</button>
+            <SignUpForm />
         </div>
     );
 };

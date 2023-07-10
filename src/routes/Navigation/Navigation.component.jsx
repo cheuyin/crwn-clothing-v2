4679a3cfcf897f1e911fbd0d7539d2262737b95a
cheuyin <yinstanleycheung@gmail.com>
2023-07-09 18:04:37 -0700

    Setup basic nav bar

---
+++ src/routes/Navigation/Navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from "react-router-dom";
+
+import "./Navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+    return (
+        <>
+            <div className="navigation">
+                <Link className="logo_container" to="/">
+                    <CrwnLogo className="logo" />
+                </Link>
+                <div className="nav_links_container">
+                    <Link className="nav_link" to="/shop">
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </>
+    );
+};
+
+export default Navigation;
